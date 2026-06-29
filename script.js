2dab0989a5a00a75011f98982885c7686d1075d0
zg461347-crypto <zg461347@gmail.com>
2026-06-30 02:52:09 +0500

    Add birthday surprise alert and loading screen logic

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () { 
+    setTimeout(() => { document.getElementById("loading").style.display = "none"; }, 2000); 
+};
+function startSurprise() { 
+    alert("Happy 17th Birthday Nomii! May your day be as amazing as you are! 🎂💖"); 
+}
